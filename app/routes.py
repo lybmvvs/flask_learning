f6a49b23dc2c7db8548735a84a97c187b5328d94
lybmvvs <stik9543@gmail.com>
2022-08-08 12:06:12 +0300

    relearning flask second lesson (templates)

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,22 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello"
+    user = {'username': 'Vova'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Ипполит'},
+            'body': 'Какая гадость эта ваша заливная рыба!!'
+        }
+    ]
+    return render_template('index.html', title = 'Home', user = user, posts = posts)
